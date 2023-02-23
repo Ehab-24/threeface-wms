68abfad7bda28bfff36995d5350f905d726bde61
Ehab-24 <ehabs1775@gmail.com>
2023-02-23 19:32:46 +0500

    Develop - Configured App.vue to show NavBar
    I added a single <main> tag, with <NavBar /> inside, in the tamplate of App.vue that will contain the main body of our application

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-    
+  <div class="h-screen px-12 py-2 bg-gradient-to-b from-slate-700 to-slate-900">
+    <NavBar />
+  </div>
 </template>
