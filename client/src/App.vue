986e27dd303f631767db0d61431d94c5135e18f9
Ehab-24 <ehabs1775@gmail.com>
2023-02-23 19:36:05 +0500

    Develop - Configured App.vue to show <NavBar>

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <div class="h-screen px-12 py-2 bg-gradient-to-b from-slate-700 to-slate-900">
+  <main class="h-screen px-12 py-2 bg-gradient-to-b from-slate-700 to-slate-900">
     <NavBar />
-  </div>
+  </main>
 </template>
