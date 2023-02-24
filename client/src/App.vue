1f6e9425acd2d98c7fdbb4a479bc5448f6b02c93
Ehab-24 <ehabs1775@gmail.com>
2023-02-24 10:52:40 +0500

    develop - App.vue:modified
    I modified the App.vue file to work with the new routes and vue-router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ProjectList from "./components/ProjectList.vue"
 import NavBar from "./components/NavBar.vue"
 
 </script>
@@ -6,5 +7,8 @@ import NavBar from "./components/NavBar.vue"
 <template>
   <main class="h-screen px-12 bg-gradient-to-b from-slate-700 to-slate-900">
     <nav-bar />
+    <body class="mt-12">
+      <project-list />
+    </body>
   </main>
 </template>
