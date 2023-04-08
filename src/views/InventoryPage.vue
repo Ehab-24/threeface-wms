caf8856643a0a4ba067f1bf3d888f95ae1b6e17b
Muhammad Amin <maminyahya@gmail.com>
2023-04-09 02:20:52 +0500

    Read operation done for Products,comments to calc total revenue

--- src/views/InventoryPage.vue
+++ src/views/InventoryPage.vue
@@ -2,16 +2,31 @@
 import ElevatedButton from "../components/ElevatedButton.vue";
 import VH1 from "../components/VH1.vue";
 import { Router, useRouter } from "vue-router";
-import Card from "../components/Card.vue";
-import MiniCard from "../components/MiniCard.vue";
 import BigCard from "../components/BigIconCard.vue";
+import ProductsTable from "../components/ProductsTable.vue";
+import { getUserState } from "../repository/authentication";
+// import {getProduct} from "../repository/db/product";
+// import { Product } from "../types/Product";
+
+// const user: any = await getUserState();
+// const products:Product[] = await getProduct(user.uid)
 
 const router: Router = useRouter();
 
 const AddProduct = (): void => {
   router.push("/add-product");
 };
-</script>
+
+// const calculatetotal = (products:Product[]): number => {
+//   let total = 0;
+//   products.forEach((product: Product) => {
+//     total += product.price * product.quantity;
+//   });
+//   return total;
+// };
+
+// const total = calculatetotal(products);
+// </script>
 
 <template>
   <v-h1>Inventory Page</v-h1>
@@ -20,7 +35,7 @@ const AddProduct = (): void => {
 
   <div class="flex flex-row">
     <div class="basis-1/4">
-      <BigCard title="Total Products" :value="6700"></BigCard>
+      <BigCard title="Total Products" :value=6700></BigCard>
     </div>
     <div class="basis-1/4">
       <BigCard title="In Stock Products" :value="12"></BigCard>
@@ -32,7 +47,10 @@ const AddProduct = (): void => {
       <BigCard title="Net Stock Value" :value="84260"></BigCard>
     </div>
   </div>
-
+  <div class="h-4"></div>
+  <Suspense>
+    <ProductsTable/>
+  </Suspense>
   <div class="h-4"></div>
   <ElevatedButton @click="AddProduct"> Add Product </ElevatedButton>
 </template>
