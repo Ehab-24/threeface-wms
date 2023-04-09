2b80b0f42ba82de2f7048d81994c91c53eaa2f44
Ehab-24 <ehabs1775@gmail.com>
2023-04-09 16:50:01 +0500

    feat - repository for invoice/mutation is completed

--- src/components/InvoiceForm.vue
+++ src/components/InvoiceForm.vue
@@ -1,36 +1,32 @@
 <script setup lang="ts">
-import { createInvoice } from '../repository/db/invoice';
-import { Invoice } from '../types/Invoice';
 import { Ref, ref } from 'vue';
-import ElevatedButton from './ElevatedButton.vue';
-import VSpinner from './VSpinner.vue';
-import { Timestamp } from '@firebase/firestore';
+
+// * Pass either 'handleCreateInvoice' or 'handleUpdateInvoice' as a prop
+defineProps<{
+    submitHandler: (e: any) => void;
+}>();
 
 const loading: Ref<boolean> = ref(false);
-const invoices: Ref<Invoice[]> = ref([]);
+// const handleUpdateInvoice = async (e: any): Promise<void> => {
+//     loading.value = true;
 
-const handleCreateInvoice = async (e: any): Promise<void> => {
-    loading.value = true;
-
-    try {
-        let invoice: Invoice = {
-            customer: e.target.elements.customer.value as string,
-            numProducts: e.target.elements.numProducts.value as number,
-            totalAmount: e.target.elements.totalAmount.value as number,
-            createdAt: Timestamp.now()
-        };
-        invoice = await createInvoice(invoice);
-        invoices.value.push(invoice);
-    }
-    catch (error) {
-        // TODO: Handle error
-        console.log(error);
-    }
-    finally {
-        loading.value = false;
-    }
-}
-
+//     try {
+//         let invoice: Invoice = {
+//             customer: e.target.elements.customer.value as string,
+//             numProducts: e.target.elements.numProducts.value as number,
+//             totalAmount: e.target.elements.totalAmount.value as number,
+//             createdAt: Timestamp.now()
+//         };
+//         await updateInvoice(invoice);
+//     }
+//     catch (error) {
+//         // TODO: Handle error
+//         console.log(error);
+//     }
+//     finally {
+//         loading.value = false;
+//     }
+// }
 </script>
 
 <template>
@@ -46,7 +42,7 @@ const handleCreateInvoice = async (e: any): Promise<void> => {
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Sign in to your account
                 </h1>
-                <form @submit.prevent="handleCreateInvoice" class="space-y-4 md:space-y-6" action="#">
+                <form @submit.prevent="submitHandler" class="space-y-4 md:space-y-6" action="#">
                     <div>
                         <label for="customer" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                             Customer
@@ -71,10 +67,8 @@ const handleCreateInvoice = async (e: any): Promise<void> => {
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-pink-500"
                             required=true>
                     </div>
-                    <v-spinner v-if="loading" />
-                    <elevated-button v-else type="submit" @click="handleCreateInvoice">
-                        Create
-                    </elevated-button>
+
+                    <slot name="submit-button"></slot>
                 </form>
             </div>
         </div>
