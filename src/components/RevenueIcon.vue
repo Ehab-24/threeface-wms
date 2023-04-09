b78f6adceaf167404108be61ba24f19c0d3f5ffe
FoxAhmad <bscs21046@itu.edu.pk>
2023-04-09 14:10:45 +0500

    Ui changes
     modified ui

--- src/components/RevenueIcon.vue
+++ src/components/RevenueIcon.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
 
-<svg class="h-8 w-8 text-white bg-opacity-6 bg-yellow-200 py-1 px-1 hover:text-yellow-500 hover:bg-gray-500 hover:fill-transparent hover:bg-white fill-yellow-600 rounded-lg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<svg class="h-8 w-8 text-white bg-opacity-6 bg-yellow-200 py-0.7 px-0.7 hover:text-yellow-500 hover:bg-gray-500 hover:fill-transparent hover:bg-white fill-yellow-600 rounded-lg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
   </svg>
   
