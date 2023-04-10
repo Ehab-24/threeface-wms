2b22834826b84c287198f07ba60de7d66e7b72bd
Muhammad Amin <maminyahya@gmail.com>
2023-04-10 11:05:04 +0500

    Removed unnecessary imports

--- src/views/CustomersPage.vue
+++ src/views/CustomersPage.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import PageHeader from '../components/PageHeader.vue';
 import CustomerTable from '../components/CustomerTable.vue';
-import ElevatedButton from "../components/ElevatedButton.vue";
 
 </script>
 
