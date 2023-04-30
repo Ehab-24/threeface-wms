59c3a93d0b85e5219e171b269dca0d6ea5f68759
FoxAhmad <bscs21046@itu.edu.pk>
2023-05-01 01:56:58 +0500

    secret resolved

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,6 @@ const port = process.env.PORT|| 3000;
 const server = http.createServer(app);
 server.listen(port, (): void => {
   console.log(`Express is listening at http://localhost:${port}`);
-  connectDB('mongodb+srv://bscs21046:ahmad37075477@cluster0.gbfhaa9.mongodb.net/?retryWrites=true&w=majority');
+  connectDB(process.env.MONGO_URL);
 });
 
